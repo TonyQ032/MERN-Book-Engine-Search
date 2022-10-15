cb62fe893cb6b24b2b843f5fef3f396b455b29d1
aq032 <anthonyq032@gmail.com>
2022-10-15 06:04:11 -0700

    Created login mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,35 @@
-// Placeholder
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`
+
+export const ADD_USER = gql`
+  mutation addUser()
+`
+
+export const SAVE_BOOK = gql`
+  mutation saveBook()
+`
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook()
+`
