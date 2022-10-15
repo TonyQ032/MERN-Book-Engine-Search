627ef7a87099687b70872e650af857c7e0ef0cab
aq032 <anthonyq032@gmail.com>
2022-10-15 16:07:34 -0700

    Created ADD_USER and SAVE_BOOK mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -20,16 +20,40 @@ export const LOGIN_USER = gql`
       }
     }
   }
-`
+`;
 
 export const ADD_USER = gql`
-  mutation addUser()
-`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook()
-`
+  mutation saveBook($data: bookData!) {
+    saveBook(data: $data) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
 
 export const REMOVE_BOOK = gql`
   mutation removeBook()
-`
+`;
